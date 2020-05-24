138a3fbcfc8497cf6bb0426647648f8c91ce3b73
willen <willengitonga.wg@gmail.com>
2020-05-24 14:14:40 +0300

    added ip address

--- referralproject/settings.py
+++ referralproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2ig)z%6dk=4tq0c7z$&nh+66v#%7*$#2av^m2bc1kie=6kdty&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eclidworkers.com','134.122.115.49']
 
 
 # Application definition
